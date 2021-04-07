c1ce57d16d859d9c4c0830ec3de98952ac5c2aa0
Steven Mancine <stevenrmancine@gmail.com>
2021-04-07 11:32:07 -0600

    Add all test to cypress

--- cypress/integration/turing_cafe_spec.js
+++ cypress/integration/turing_cafe_spec.js
@@ -9,18 +9,29 @@ describe('Turing Cafe', () => {
 
   it('Should have reservation cards', () =>{
     cy.get('.reservations > .reservation-card').contains('Christie')
-    cy.get('.reservations > .reservation-card').contains('12/29')
-    cy.get('.reservations > .reservation-card').contains('7:00')
-    cy.get('.reservations > .reservation-card').contains('Number of guest: 12')
-    cy.get('.reservation-card__btn').contains('Cancel')
-  })
+      .get('.reservations > .reservation-card').contains('12/29')
+      .get('.reservations > .reservation-card').contains('7:00')
+      .get('.reservations > .reservation-card').contains('Number of guest: 12')
+      .get('.reservation-card__btn').contains('Cancel')
+  });
 
   it('Should have a form with inputs', () =>{
     cy.get('form > input[name="name"]')
-    cy.get('form > input[name="time"]')
-    cy.get('form > input[name="date"]')
-    cy.get('form > input[name="number"]')
+      .get('form > input[name="time"]')
+      .get('form > input[name="date"]')
+      .get('form > input[name="number"]')
+      .get('form > button').contains('Make Reservation')
+  });
+
+  it('Should be able to type into the form and submit new resy to be displayed', () => {
+    cy.get('form > input[name="name"]').type('Steven').should('have.value', 'Steven')
+      .get('form > input[name="date"]').type('4/19').should('have.value', '4/19')
+      .get('form > input[name="time"]').type('7:13').should('have.value', '7:13')  
+      .get('form > input[name="number"]').type('5').should('have.value', '5')
+      .get('form > button').click()
+    cy.get('.reservations > .reservation-card').contains('Steven')
+      .get('.reservations > .reservation-card').contains('4/19')
+      .get('.reservations > .reservation-card').contains('7:13')
+      .get('.reservations > .reservation-card').contains('Number of guest: 5')
   })
-
-
 })
